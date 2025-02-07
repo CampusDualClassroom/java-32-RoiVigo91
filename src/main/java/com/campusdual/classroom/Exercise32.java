4c4e68745c62d36f94ab22d662a73931b7d9e53f
RoiVigo91 <roioliveira71@gmail.com>
2025-02-07 14:29:27 +0100

    Ejercicio32Resuelto

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -3,17 +3,20 @@ package com.campusdual.classroom;
 import com.campusdual.util.Utils;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
+        String texto = generateStringToSave(null);
 
+        printToFile(texto);
     }
 
     public static String generateStringToSave(String string) {
-
+        return (string != null) ? string : generateUserInputToSave();
     }
 
     private static String generateUserInputToSave(){
@@ -27,8 +30,16 @@ public class Exercise32 {
     }
 
     public static void printToFile(String string) {
+        String rutaFichero = "src/main/resources/data.txt";
 
+        try (FileWriter writer = new FileWriter(rutaFichero)) {
+            writer.write(string);
+            System.out.println("Texto guardado correctamente en " + rutaFichero);
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
     }
+
 
 
 }
